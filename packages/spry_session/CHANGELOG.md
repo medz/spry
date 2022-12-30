894f4730fd9a44e3f15831c42063e3558b551f00
Seven Du <whine.whitish.0u@icloud.com>
2022-12-31 00:08:58 +0800

    chore(spry_session): Adapt to spry 0.1.2

--- packages/spry_session/CHANGELOG.md
+++ packages/spry_session/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.2
+
+Adapt to spry 0.1.2
+
 ## 0.0.2
 
 - Update `spry` dependency to `0.0.5`.
--- packages/spry_session/lib/src/context_session_extension.dart
+++ packages/spry_session/lib/src/context_session_extension.dart
@@ -6,5 +6,6 @@ import 'package:spry/spry.dart';
 /// Spry [Context] extension for [HttpSession] management.
 extension ContextSessionExtension on Context {
   /// Returns the [HttpSession] for this [Context].
-  HttpSession get session => (get(SPRY_HTTP_REQUEST) as HttpRequest).session;
+  HttpSession get session =>
+      (get(SPRY_HTTP_ORIGIN_REQUEST) as HttpRequest).session;
 }

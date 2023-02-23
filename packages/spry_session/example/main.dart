92762cab1e582105a5dbf16a03b4433cba48629f
Seven Du <whine.whitish.0u@icloud.com>
2023-02-24 05:55:51 +0800

    chore: Fix example

--- packages/spry_session/example/main.dart
+++ packages/spry_session/example/main.dart
@@ -11,7 +11,7 @@ void main() async {
 
     session['foo'] = 'bar';
 
-    context.response.send('Stored foo in session, Session ID: ${session.id}');
+    context.response.text('Stored foo in session, Session ID: ${session.id}');
   }
 
   await spry.listen(handler, port: 3000);

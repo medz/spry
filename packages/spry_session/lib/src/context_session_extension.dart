5c75343429309c5261f878479ae78869b9acb19c
Seven Du <whine.whitish.0u@icloud.com>
2023-03-17 22:56:31 +0800

    feat: Adapt to Spry 1.0

--- packages/spry_session/lib/src/context_session_extension.dart
+++ packages/spry_session/lib/src/context_session_extension.dart
@@ -1,11 +1,9 @@
 import 'dart:io';
 
-import 'package:spry/constants.dart';
 import 'package:spry/spry.dart';
 
 /// Spry [Context] extension for [HttpSession] management.
 extension ContextSessionExtension on Context {
   /// Returns the [HttpSession] for this [Context].
-  HttpSession get session =>
-      (get(SPRY_HTTP_ORIGIN_REQUEST) as HttpRequest).session;
+  HttpSession get session => this[HttpRequest].session;
 }

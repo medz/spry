5a7d444fef6c61bea2a27231a62d6404381aa596
Seven Du <shiweidu@outlook.com>
2024-12-30 14:22:07 +0800

    docs: *

--- lib/src/http/request.dart
+++ lib/src/http/request.dart
@@ -1,6 +1,8 @@
 import 'http_message.dart';
 
+/// HTTP request.
 class Request<T> extends HttpMessage {
+  /// Creates a new [Request] instance.
   Request({
     required String method,
     required this.url,
@@ -9,7 +11,19 @@ class Request<T> extends HttpMessage {
     required this.runtime,
   }) : method = method.toUpperCase();
 
+  /// Returns the request method.
+  ///
+  /// > [!NOTE]
+  /// >
+  /// > The method value is upper-case.
   final String method;
+
+  /// Returns the request resolved url.
   final Uri url;
+
+  /// Returns runtime platform native request value.
+  ///
+  /// - If platform is dart, the [runtime] is `HttpRequest`
+  /// - If platform is Bun/Node/Deno, the [runtime] is web `Request`.
   final T runtime;
 }

48ed654e06c9a7d5fea73789ca3b77eb2b1c44d0
Seven Du <shiweidu@outlook.com>
2024-12-30 14:14:46 +0800

    docs: headers

--- lib/src/http/headers.dart
+++ lib/src/http/headers.dart
@@ -1,7 +1,22 @@
 import '_utils.dart';
 
+/// The HTTP [Headers].
+///
+/// {@template spry.http.headers.example}
+/// ```dart
+/// final headers = Headers({'a': '1'});
+///
+/// print(headers.get('a')); // Prints: 1
+/// ```
+/// {@endtemplate}
 extension type Headers._(List<(String, String)> _headers)
     implements Iterable<(String, String)> {
+  /// Creates a new [Headers].
+  ///
+  /// - [init]: The headers init map.
+  ///
+  /// Example:
+  /// {@macro spry.http.headers.example}
   factory Headers([Map<String, String>? init]) {
     final headers = Headers._([]);
     if (init != null && init.isNotEmpty) {
@@ -13,6 +28,7 @@ extension type Headers._(List<(String, String)> _headers)
     return headers;
   }
 
+  /// Check if a header name exists in the [Headers].
   bool has(String name) {
     final normalizedName = normalizeHeaderName(name);
     for (final (name, _) in this) {
@@ -39,10 +55,12 @@ extension type Headers._(List<(String, String)> _headers)
     }
   }
 
+  /// Adds a new header key-value.
   void add(String name, String value) {
     _headers.add((normalizeHeaderName(name), value));
   }
 
+  /// Sets a header, If the name exists remove it.
   void set(String name, String value) {
     final normalizedName = normalizeHeaderName(name);
     _headers
@@ -50,6 +68,10 @@ extension type Headers._(List<(String, String)> _headers)
       ..add((normalizedName, value));
   }
 
+  /// Remove a header.
+  ///
+  /// If [value] is provided, delete the exact match.
+  /// Otherwise, delete all values of [name].
   void remove(String name, [String? value]) {
     final normalizedName = normalizeHeaderName(name);
     bool test((String, String) e) {

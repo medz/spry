28af56f5d23d9c8bb66e54c2161229c614e01809
Seven Du <5564821+medz@users.noreply.github.com>
2026-03-30 20:51:58 +0800

    chore(docs): upgrade vitepress to 2.0 alpha (#185)
    * chore(docs): upgrade vitepress to 2.0 alpha
    
    * fix(docs): harden vitepress 2 upgrade

--- sites/spry.medz.dev/.vitepress/theme/components/HomeHeroVisual.vue
+++ sites/spry.medz.dev/.vitepress/theme/components/HomeHeroVisual.vue
@@ -13,7 +13,7 @@
       </div>
     </div>
     <div class="spry-window__body">
-      <div class="language-dart vp-adaptive-theme">
+      <div class="language-dart spry-window__code">
         <pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">import</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;"> 'package:spry/spry.dart'</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">;</span></span>
 <span class="line"></span>
 <span class="line"><span style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;">Response</span><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;"> handler</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">(</span><span style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;">Event</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> event) {</span></span>
